5c9530896c67f9e79d5130e6c79147cab6075f8a
Aleksandr Heletukha <heletukha@gmail.com>
2020-11-23 14:59:29 +0200

    add gif

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from "./LocationMarker"
+import LocationInfoBox from "./LocationInfoBox"
 
 const Map = ({ eventData, center, zoom }) => {
+    const [locationInfo, setLocationInfo] = useState(null)
+
     const markers = eventData.map(e => {
         if (e.categories[0].id === 8){
-            return <LocationMarker lat={e.geometries[0].coordinates[1]} lng={e.geometries[0].coordinates[0]}/>
+            return <LocationMarker lat={e.geometries[0].coordinates[1]} lng={e.geometries[0].coordinates[0]} onClick={() => {
+                setLocationInfo({id: e.id, title: e.title})
+            }
+            }/>
         }
         return null
     })
@@ -18,6 +25,7 @@ const Map = ({ eventData, center, zoom }) => {
             >
                 {markers}
             </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo}/>}
         </div>
     )
 }
